4765d5a051768f3519cdc0e163975382145b0042
jenniferdibenedetto <jennifer.dibenedetto00@gmail.com>
2025-11-25 12:23:29 +0100

    protocol.h

--- server-project/src/protocol.h
+++ server-project/src/protocol.h
@@ -9,16 +9,22 @@
 #define PROTOCOL_H_
 
 // Shared application parameters
-#define SERVER_PORT 27015  // Server port (change if needed)
-#define BUFFER_SIZE 512    // Buffer size for messages
-#define QUEUE_SIZE 5       // Size of pending connections queue
+#define SERVER_PORT 56700
+#define BUFFER_SIZE 512
+#define QUEUE_SIZE 6     
 
-// Function prototypes
-// Add here the signatures of the functions implemented by students
+//DEFINIZIONE STRUTTURE DATI 
+// Struttura per la richiesta (Client -> Server)
+typedef struct {
+    char type;        // Tipo dati: 't', 'h', 'w', 'p'
+    char city[64];    // Nome citta
+} weather_request_t;
 
-/*
- * Example function to implement:
- * int handle_client(int client_socket);
- */
+// Struttura per la risposta (Server -> Client)
+typedef struct {
+    unsigned int status;  // 0=OK, 1=Err Citta, 2=Err Richiesta
+    char type;            // Eco del tipo
+    float value;          // Valore meteo
+} weather_response_t;
 
 #endif /* PROTOCOL_H_ */
